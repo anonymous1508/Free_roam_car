836f852653a84424b3c4740a0e9c606c8a6061be
anonymous1508 <109167785+anonymous1508@users.noreply.github.com>
2023-09-03 11:04:03 +0530

    manual free roam car

---
+++ .history/controls_20230903102458.js
@@ -0,0 +1,40 @@
+class Controls{
+    constructor(){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        this.#addkeyboardlisteners();
+    }
+    #addkeyboardlisteners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                this.left=true;
+                case "ArrowRight":
+                this.right=true;
+                case "ArrowUp":
+                this.forward=true;
+                case "ArrowDown":
+                this.reverse=true;
+                break;
+
+            }
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                this.left=false;
+                case "ArrowRight":
+                this.right=false;
+                case "ArrowUp":
+                this.forward=false;
+                case "ArrowDown":
+                this.reverse=false;
+                break;
+
+            }
+        }
+    }
+}
---
+++ .history/main_20230903100442.js
@@ -0,0 +1,6 @@
+const canvas=document.getElementById("myCanvas");
+canvas.height=window.innerHeight;
+canvas.width=200;
+
+
+const ctx=canvas.getContext("2d");
